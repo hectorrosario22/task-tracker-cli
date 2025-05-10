371cafdd410c4aa277d01f36bace2ac45fa32f2c
Héctor Rosario <36975677+hectorrosario22@users.noreply.github.com>
2025-05-10 18:51:33 -0400

    feat: implement task update functionality and refactor add task method

--- Program.cs
+++ Program.cs
@@ -15,6 +15,9 @@ switch (command)
     case "add":
         await AddCommand(commandArgs);
         break;
+    case "update":
+        await UpdateCommand(commandArgs);
+        break;
     default:
         Console.WriteLine("Invalid command.");
         break;
@@ -24,11 +27,43 @@ async Task AddCommand(string[] commandArgs)
 {
     if (commandArgs.Length != 1)
     {
+        Console.WriteLine("Please provide a task description.");
         return;
     }
 
-    // TODO: Add to JSON file
-    TrackerTask newTask = new(commandArgs[0]);
-    var id = await taskService.AddTask(newTask);
+    if (string.IsNullOrWhiteSpace(commandArgs[0]))
+    {
+        Console.WriteLine("Task description cannot be empty.");
+        return;
+    }
+
+    var id = await taskService.AddTask(commandArgs[0]);
     Console.WriteLine($"Task added successfully (ID: {id})");
+}
+
+async Task UpdateCommand(string[] commandArgs)
+{
+    if (commandArgs.Length != 2)
+    {
+        Console.WriteLine("Please provide a task ID and a new description.");
+        return;
+    }
+
+    if (!int.TryParse(commandArgs[0], out int id))
+    {
+        Console.WriteLine("Invalid task ID.");
+        return;
+    }
+
+    if (string.IsNullOrWhiteSpace(commandArgs[1]))
+    {
+        Console.WriteLine("Task description cannot be empty.");
+        return;
+    }
+
+    var (Success, ErrorMessage) = await taskService.UpdateTask(id, commandArgs[1]);
+    if (!Success)
+    {
+        Console.WriteLine(ErrorMessage);
+    }
 }
--- TaskService.cs
+++ TaskService.cs
@@ -20,7 +20,7 @@ public class TaskService
         }
     }
 
-    public async Task<int> AddTask(TrackerTask task)
+    public async Task<int> AddTask(string description)
     {
         using FileStream stream = File.Open(
             _filepath, FileMode.Open,
@@ -28,8 +28,11 @@ public class TaskService
         );
         var tasks = await JsonSerializer.DeserializeAsync<List<TrackerTask>>(stream, _jsonOptions) ?? [];
 
-        task.Id = tasks.Count == 0 ? 1 : tasks.Max(d => d.Id) + 1;
-        task.CreatedAt = DateTime.Now;
+        TrackerTask task = new(description)
+        {
+            Id = tasks.Count == 0 ? 1 : tasks.Max(d => d.Id) + 1,
+            CreatedAt = DateTime.Now,
+        };
         tasks.Add(task);
 
         stream.SetLength(0);
@@ -37,5 +40,35 @@ public class TaskService
 
         await JsonSerializer.SerializeAsync(stream, tasks, _jsonOptions);
         return task.Id;
+    }
+
+    public async Task<(bool Success, string? ErrorMessage)> UpdateTask(int id, string description)
+    {
+        using FileStream stream = File.Open(
+            _filepath, FileMode.Open,
+            FileAccess.ReadWrite, FileShare.None
+        );
+        var tasks = await JsonSerializer.DeserializeAsync<List<TrackerTask>>(stream, _jsonOptions) ?? [];
+
+        var index = tasks.FindIndex(t => t.Id == id);
+        if (index == -1)
+        {
+            return (false, $"Task with ID {id} not found.");
+        }
+
+        var task = tasks.First(t => t.Id == id);
+        task = task with
+        {
+            Description = description,
+            UpdatedAt = DateTime.Now
+        };
+
+        tasks[index] = task;
+
+        stream.SetLength(0);
+        stream.Position = 0;
+
+        await JsonSerializer.SerializeAsync(stream, tasks, _jsonOptions);
+        return (true, null);
     }
 }
